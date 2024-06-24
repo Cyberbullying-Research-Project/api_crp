94959ac0bf4a966412bc7d603c861b91d12b1e01
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2024-06-24 05:54:46 -0500

    Despliegue server

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './socket-io/redis-io.adapter';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  
-  app.setGlobalPrefix('api/v1'); 
-  app.useGlobalPipes(new ValidationPipe()); 
-  
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+
   // Redis Websockets
-  const redisIoAdapter = new RedisIoAdapter(app);
-  await redisIoAdapter.connectToRedis();
+  // const redisIoAdapter = new RedisIoAdapter(app);
+  // await redisIoAdapter.connectToRedis();
 
-  app.useWebSocketAdapter(redisIoAdapter);
-
+  // app.useWebSocketAdapter(redisIoAdapter);
 
   await app.listen(4000);
   console.log(`Application is running on: ${await app.getUrl()}`);
